1c4f9b0fd9d4f3a25852d6cdade6489842dcbdd4
ivan@360zebra.com <LehuaYana521>
2018-05-19 22:18:18 +0800

    向Index页面添加分页

--- HiNetCoreDemo/Pages/Student/Index.cshtml.cs
+++ HiNetCoreDemo/Pages/Student/Index.cshtml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using HiNetCoreDemo.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -24,16 +23,27 @@ namespace HiNetCoreDemo.Pages.Student
         public string NameSortParm { get; set; }        
         [TempData]
         public string IdSortParm { get; set; }
-
+        [TempData]
+        public string CurrentSort { get; set; }
         [TempData]
         public string CurrentFilter { get; set; }
-        public async Task<IActionResult> OnGet(string sortOrder,string currentFilter)
+
+        public int PageIndex { get; set; }
+        public int TotalPages { get; set; }
+
+        public bool HasPreviousPage => PageIndex > 1;
+
+        public bool HasNextPage => PageIndex < TotalPages;
+
+        public async Task<IActionResult> OnGet(string sortOrder,string currentFilter,int currentPageIndex=1)
         {
             //Students = _context.Student.Include(m => m.Subjects).OrderBy(x => x.StId).ToList();
 
+            TempData["CurrentSort"] = sortOrder;
+            TempData["CurrentFilter"] = currentFilter;
+
             TempData["IdSortParm"] = string.IsNullOrEmpty(sortOrder) ? "id_desc" : "";
             TempData["NameSortParm"] = sortOrder == "Name" ? "name_desc" : "Name";
-            TempData["CurrentFilter"] = currentFilter;
 
             var students = from m in _context.Student.Include(m => m.Subjects)
                            select m;
@@ -56,19 +66,28 @@ namespace HiNetCoreDemo.Pages.Student
                     students = students.OrderBy(x => x.StId);
                     break;
             }
-            Students = await students.ToListAsync();
+            //Students = await students.ToListAsync();
+
+            int pageSize = 3;
+            var count = await students.CountAsync();
+            var items = await students.Skip((currentPageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            PageIndex = currentPageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            Students = items;
             return Page();
         }
-
-        public async Task<IActionResult> OnPostFilterAsync(string sortOrder, string searchString)
+        public async Task<IActionResult> OnPostFilterAsync(string sortOrder, string searchString, int currentPageIndex = 1)
         {
-            TempData["IdSortParm"] = string.IsNullOrEmpty(sortOrder) ? "id_desc" : "";
-            TempData["NameSortParm"] = sortOrder == "Name" ? "name_desc" : "Name";
+            TempData["CurrentSort"] = sortOrder;
             TempData["CurrentFilter"] = searchString;
 
+            TempData["IdSortParm"] = string.IsNullOrEmpty(sortOrder) ? "id_desc" : "";
+            TempData["NameSortParm"] = sortOrder == "Name" ? "name_desc" : "Name";
+
+            
             var students = from m in _context.Student.Include(x => x.Subjects).OrderBy(x => x.StId)
                            select m;
-
+            
             if (!string.IsNullOrEmpty(searchString))
             {
                 students = students.Where(m => m.StId.Contains(searchString) || m.StName.Contains(searchString));
@@ -88,9 +107,15 @@ namespace HiNetCoreDemo.Pages.Student
                     students = students.OrderBy(x => x.StId);
                     break;
             }
-
-            Students = await students.ToListAsync();
+            //Students = await students.ToListAsync();
+            int pageSize = 3;
+            var count = await students.CountAsync();
+            var items = await students.Skip((currentPageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            PageIndex = currentPageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            Students = items;
             return Page();
         }
+
     }
 }
